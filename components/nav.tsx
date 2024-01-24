8519811c56c23f3efd8f448637c0a3d1925ef956
Ronnie Nayak <abhishek.ron.nayak@gmail.com>
2024-01-24 14:00:25 +0530

    adding loading component

--- components/nav.tsx
+++ components/nav.tsx
@@ -5,14 +5,15 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { Loading } from "./loading";
 
 export default function Nav() {
   const [centerLogo, setCenterLogo] = useState(false)
   const [options, setOptions] = useState(false)
   const pathname = usePathname()
   const searchParams = useSearchParams()
-  const setPageScroll = useSetRecoilState(PageScrollState)
+  const [pageScroll, setPageScroll] = useRecoilState(PageScrollState)
   // const router = useRouter()
   useEffect(() => {
     if ("/" === pathname) {
@@ -32,13 +33,16 @@ export default function Nav() {
   return (
     <nav className="bg-[#F5F5F5] fixed w-screen top-0 left-0 z-50 p-1 sm:p-0">
       <ul className="flex items-center justify-center sm:justify-end gap-5 sm:mx-24 py-6 sm:text-[1.5vw] font-semi relative">
-        <motion.div>
+        <div className={`mr-auto z-50 h-28 absolute -top-10 ${centerLogo ? "hidden sm:block left-0" : "left-1/2 -translate-x-1/2"} transition-all duration-700 bg-cream px-10`}>
           <Link href="/">
-            <img className={`mr-auto h-40 absolute -top-10 ${centerLogo ? "hidden sm:block left-0" : "left-1/2 -translate-x-1/2"} transition-all duration-700`} src='/arn.jpeg' alt='logo' style={{
+            <img className="h-40" src='/arn.jpeg' alt='logo' style={{
               clipPath: "inset(30% 10% 30% 10%)"
             }} />
           </Link>
-        </motion.div>
+        </div>
+        <div className={` ${!pageScroll ? "absolute  sm:block" : "hidden"}  left-1/2 -translate-x-1/2 z-40 px-20 w-max bg-cream`}>
+          <Loading />
+        </div>
         <button onClick={() => handleScroll("./#hero")} className={`${options ? "" : "-translate-y-20"} transition-all duration-700`}>Home</button>
         <button onClick={() => handleScroll("./#projects")} className={`${options ? "" : "-translate-y-20"} transition-all duration-700`}>Projects</button>
         <button onClick={() => handleScroll("./#about")} className={`${options ? "" : "-translate-y-20"} transition-all duration-700`}>About</button>
